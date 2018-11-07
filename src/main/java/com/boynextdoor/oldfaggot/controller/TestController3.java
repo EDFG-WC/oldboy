cd56572696911792cf46dbaabd21c3ba24ba2784
王晨 <wangchen706@hotmail.com>
2018-11-08 00:10:12 +0800

    11.08更新

--- src/main/java/com/boynextdoor/oldfaggot/controller/TestController3.java
+++ src/main/java/com/boynextdoor/oldfaggot/controller/TestController3.java
@@ -1,18 +1,50 @@
 package com.boynextdoor.oldfaggot.controller;
 
-import com.boynextdoor.oldfaggot.entity.BaiduEntity;
-import javax.annotation.Resource;
+
+import com.boynextdoor.oldfaggot.entity.User;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Auther: wangc
  * @Date: 2018/10/24 22:00
  * @Description:
  */
-@RestController
+@Controller
 @RequestMapping("/test3")
 public class TestController3 {
 
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @GetMapping("/login")
+  @ResponseBody
+  public Map<String, Object> login(HttpServletRequest request, HttpServletResponse response) {
+    logger.trace("日志输出 trace");
+    logger.debug("日志输出 debug");
+    logger.info("日志输出 info");
+    logger.warn("日志输出 warn");
+    logger.error("日志输出 error");
+    Map<String,Object> map =new HashMap<String,Object>();
+    String name=request.getParameter("name");
+    String address=request.getParameter("address");
+    if(!name.equals("") && address!=""){
+      //User user =new User(userName,password);
+      User user = new User();
+      user.setName(name);
+      user.setAddress(address);
+      request.getSession().setAttribute("user",user);
+      map.put("result","1");
+    }else{
+      map.put("result","0");
+    }
+    return map;
+  }
 }
